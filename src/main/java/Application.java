00df0d4603a9b70d7083743d4aadeed3f1c024d5
Polyxen1a <maslowa@inbox.ru>
2023-07-15 23:17:37 +0300

    JDBC (повторение) 15.07.23

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,51 +1,49 @@
 import java.sql.*;
 import java.util.List;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Application {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws SQLException {
         final String user = "postgres";
-        final String password = "4658686ghcj4745";
+        final String pass = "root";
         final String url = "jdbc:postgresql://localhost:5432/skypro";
-
-
-        try (final Connection connection =
-                     DriverManager.getConnection(url, user, password);
-             PreparedStatement statement =
-                     connection.prepareStatement("SELECT * FROM books")) {
-
-            ResultSet resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-                int idOfBook = resultSet.getInt("book_id");
-                System.out.println("ID книги: " + idOfBook);
-
-                String titleOfBook = resultSet.getString("title");
-                int authorOfBook = resultSet.getInt("author_id");
-                int amountOfBook = resultSet.getInt("amount");
-
-                System.out.println("Название: " + titleOfBook);
-                System.out.println("ID автора: " + authorOfBook);
-                System.out.println("Количество: " + amountOfBook);
+        try (final Connection connection = DriverManager.getConnection(url, user, pass);
+             PreparedStatement statement = connection.prepareStatement("" +
+                     "SELECT  * FROM employee WHERE id = (?)")) {
+            statement.setInt(1, 2);
+            final ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String name = "Name: " + resultSet.getString("first_name");
+                String surname = "Surname: " + resultSet.getString("last_name");
+                String gender = "Gender: " + resultSet.getString(4);
+                String city = "City id: " + resultSet.getInt(6);
+                System.out.println(name);
+                System.out.println(surname);
+                System.out.println(gender);
+                System.out.println(city);
             }
 
-        } catch (SQLException e) {
-            System.out.println("Ошибка при подключении к БД!");
-            e.printStackTrace();
-        }
-        public class Application2 {
+            EmployeeDao employeeDao = new EmployeeDaoImpl();
+            List <Employee> employees  = employeeDao.getAllEmployee();
 
-            public static void main(String[] args) {
-                BookDAO bookDAO = new BookDAOImpl();
-                List<Book> books = bookDAO.getAllBooks();
-
-                for (Book book : books) {
-                    System.out.println("Book ID: " + book.getId());
-                    System.out.println("Title: " + book.getTitle());
-                    System.out.println("Author_id: " + book.getAuthorId());
-                    System.out.println("Amount: " + book.getAmount());
-                }
+            for (Employee employee : employees) {
+                System.out.println("Id: " + employee.getId());
+                System.out.println("Name: " + employee.getFirst_name());
+                System.out.println("Surname: " + employee.getLast_name());
+                System.out.println("Gender: " + employee.getGender());
+                System.out.println("City id: " + employee.getCity_id());
+                System.out.println("Age: " + employee.getAge());
+                System.out.println("----------------------------------");
             }
+            Employee john = new Employee(7,"john", "Washington", "male", 44,1);
+//            employeeDao.createEmployee(john);
+//            employeeDao.deleteEmployee(7);
+//            employeeDao.updateEmployee(4, john);
+
+            System.out.println(employeeDao.getById(3).getFirst_name());
         }
     }
 }
--- src/main/java/UserDaoImpl.java
+++ src/main/java/UserDaoImpl.java
@@ -1,32 +1,115 @@
-public class UserDaoImpl {
-    implements UserDao {
+import com.sun.jdi.connect.spi.Connection;
 
-        // Тут может быть код для подключения к источнику данных
-        // Например, к базе данных
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-        @Override
-        public List<User> getAllUsers() {
-      ... // Реализуйте логику получения всех пользователей
+public class UserDaoImpl implements EmployeeDao {
+    private final String user = "postgres";
+    private final String pass = "root";
+    private final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+    @Override
+    public List<Employee> getAllEmployee () {
+      List<Employee> employees = new ArrayList<>();
+
+      try (final Connection connection = DriverManager.getConnection(url, user, pass);
+           PreparedStatement statement = connection.prepareStatement("SELECT * FROM employee")) {
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+          int id = resultSet.getInt("id");
+          String first_name = resultSet.getString("first_name");
+          String last_name = resultSet.getString("last_name");
+          String gender = resultSet.getString("gender");
+          int age = resultSet.getInt("age");
+          int city_id = resultSet.getInt("city_id");
+          employees.add(new Employee(id, first_name, last_name, gender, age, city_id));
         }
-
-        @Override
-        public User getUserById(int id) {
-      ... // Реализуйте логику получения пользователя по id
-        }
-
-        @Override
-        public void createUser(User user) {
-      ... // Реализуйте логику создания нового пользователя
-        }
-
-        @Override
-        public void updateUser(User user) {
-      ... // Реализуйте логику обновления существующего пользователя
-        }
-
-        @Override
-        public void deleteUser(int id) {
-      ... // Реализуйте логику удаления пользователя по id
-        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+      return employees;
     }
+
+    @Override
+    public Employee getById ( int id){
+      Employee employee = new Employee();
+      try (final Connection connection = DriverManager.getConnection(url, user, pass);
+           PreparedStatement statement = connection.prepareStatement(
+                   "SELECT * FROM employee WHERE id = (?)")) {
+
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+          employee.setId(resultSet.getInt(1));
+          employee.setFirst_name(resultSet.getString("first_name"));
+          employee.setLast_name(resultSet.getString("last_name"));
+          employee.setGender(resultSet.getString("gender"));
+          employee.setAge(resultSet.getInt(5));
+          employee.setCity_id(resultSet.getInt("city_id"));
+
+        }
+
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
+      return employee;
+    }
+
+    @Override
+    public void createEmployee (Employee employee){
+      try (final Connection connection = DriverManager.getConnection(url, user, pass);
+           PreparedStatement statement = connection.prepareStatement(
+                   "INSERT INTO employee (first_name, last_name, gender, age, city_id) VALUES ( (?), (?), (?), (?), (?))")) {
+
+        statement.setString(1, employee.getFirst_name());
+        statement.setString(2, employee.getLast_name());
+        statement.setString(3, employee.getGender());
+        statement.setInt(4, employee.getAge());
+        statement.setInt(5, employee.getCity_id());
+        statement.executeUpdate();
+
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+
+    @Override
+    public void updateEmployee ( int id, Employee employee){
+      try (final Connection connection = DriverManager.getConnection(url, user, pass);
+           PreparedStatement statement = connection.prepareStatement(
+                   "UPDATE employee SET first_name= (?), last_name = (?), gender = (?), age = (?), city_id = (?) WHERE id = (?)")) {
+//            statement.setInt(1, employee.getId());
+        statement.setString(1, employee.getFirst_name());
+        statement.setString(2, employee.getLast_name());
+        statement.setString(3, employee.getGender());
+        statement.setInt(4, employee.getAge());
+        statement.setInt(5, employee.getCity_id());
+        statement.setInt(6, id);
+        statement.executeUpdate();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+
+
+    }
+
+    @Override
+    public void deleteEmployee ( int id){
+      try (final Connection connection = DriverManager.getConnection(url, user, pass);
+           PreparedStatement statement = connection.prepareStatement(
+                   "DELETE FROM employee WHERE id = (?)")) {
+        statement.setInt(1, id);
+        statement.executeUpdate();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+
+
+    }
+  }
 }
